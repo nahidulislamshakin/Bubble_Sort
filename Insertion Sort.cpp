048925df644226f77fa270f8b9bd12d7eb6e2538
Nahidul Islam Shakin <shakinhabib2000@gmail.com>
2023-06-05 17:43:53 +0600

    Insertion Sort added

---
+++ Insertion Sort.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void InsertionSort(int a[], int n)
+{
+    int i, j, temp;
+    for(i=1;i<n;i++){
+        temp = a[i];
+        j=i-1;
+        while(j>=0 && a[j]>temp){
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=temp;
+
+    }
+}
+
+int main()
+{
+
+        int n;
+        cin>>n;
+         int a[n];
+        for(int i=0;i<n;i++)
+          cin>>a[i];
+            InsertionSort(a,n);
+            for(int i=0;i<n;i++)
+            cout<<a[i]<<endl;
+    return 0;
+}
+
